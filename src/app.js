5df81b64129b061e462959e13118f2e3c785e531
nicocabrerafit2 <nicodcabrera@outlook.com>
2025-04-03 20:38:28 -0300

    agrego uso de variables de entorno

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import apiRoutes from './routes/index.js';
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -9,11 +11,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Bienvenido a la API de Ferretería' });
+    res.json({
+        message: 'Bienvenido a la API de Ferretería',
+        description: 'Esta API permite gestionar productos, pedidos y clientes de forma eficiente.',
+        endpoints: [
+            { method: 'GET', route: '/api/products', description: 'Obtiene la lista de productos' },
+            { method: 'GET', route: '/api/orders', description: 'Obtiene la lista de pedidos' },
+            { method: 'GET', route: '/api/customers', description: 'Obtiene la lista de clientes' }
+        ],
+        documentation: 'xxxx'
+    });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
