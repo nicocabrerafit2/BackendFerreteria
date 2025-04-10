ea2f11a55e07c79be612641ff1389fef238754fe
nicocabrerafit2 <nicodcabrera@outlook.com>
2025-04-10 10:57:57 -0300

    agrego los models y comienzo a escribir los schemas

---
+++ src/persistence/mongoDB/models/product.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true 
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0 
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0 
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  images: {
+    type: [String],
+    validate: {
+      validator: function(urls) {
+        
+        return urls.every(url => /^https?:\/\/.*$/.test(url));
+      },
+      message: 'Cada imagen debe ser una URL válida.'
+    }
+  },
+  sku: {
+    type: String,
+    required: true,
+    unique: true, 
+    trim: true
+  }
+}, {
+  timestamps: true 
+});
+
+module.exports = mongoose.model('Product', productSchema);
